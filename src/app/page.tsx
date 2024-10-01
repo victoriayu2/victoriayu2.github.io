4a6c5789e3a301ffc3f89bf22e3f65ab180d150d
Victoria Yu <47955224+victoriayu2@users.noreply.github.com>
2024-09-30 22:31:13 -0400

    Changed image to be more circular

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,18 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-white p-6">
       <div className="flex flex-col md:flex-row items-center space-y-6 md:space-x-6">
-        <div className="relative w-48 h-48 md:w-64 md:h-64 overflow-hidden rounded-full">
+        <div className="relative w-24 h-24 md:w-32 md:h-32 overflow-hidden rounded-full">
           <Image
             src={myImage}
             alt="Headshot of Victoria Yu"
             layout="fill"
             objectFit="cover"
+            className="rounded-full"
           />
         </div>
 
-        <div className="flex flex-col text-left md:text-left">
-          <h1 className="text-4xl font-bold text-black mb-2">
+        <div className="flex flex-col text-left md:text-left flex-1 ml-2"> {}
+          <h1 className="text-2xl md:text-4xl font-bold text-black mb-2">
             Hi, my name is Victoria!
           </h1>
           <p className="text-lg text-black">
